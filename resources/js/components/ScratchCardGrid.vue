8741cc93436abd8821cfaac8fbab34d544b6328a
bsamworthdev <bsamworthdev@outlook.com>
2020-04-12 09:49:14 +0000

    First commit

---
+++ resources/js/components/ScratchCardGrid.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    
+                    <scratch-card></scratch-card>
+                    <scratch-card></scratch-card>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import scratchCard from './ScratchCard';
+    export default {
+        components : {
+            scratchCard,
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
