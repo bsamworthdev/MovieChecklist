f19dee2103be0b31ed457bb5284d0e9d344569f2
bsamworthdev <bsamworthdev@outlook.com>
2020-05-04 16:23:11 +0000

    Added IMDB modal vue

---
+++ resources/js/components/IMDBModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <modal @close="close">
+    <div slot="header">
+      <button type="button" class="close" @click="$emit('close')" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <h5 class="modal-title">IMDB</h5>
+    </div>
+
+    <div slot="body">
+      <div class="container">
+        hello world
+        </div>
+    </div>
+
+    <div slot="footer">
+      <button type="button" class="btn btn-default" @click="$emit('close')">Close</button>
+    </div>
+  </modal>
+</template>
+
+<script>
+import modal from "./Modal";
+export default {
+  props: {
+    parentData: Object,
+    movie: Object
+  },
+  components: {
+    modal
+  },
+  mounted() {
+    console.log("Component mounted.");
+  },
+  methods: {
+    close: function() {
+      this.$emit("close");
+    }
+  },
+  data() {
+    return {
+      modalId: "IMDBModal"
+    };
+  }
+};
+</script>
+<style scoped>
+
+</style>
