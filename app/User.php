0370af0371a0bb7dffb28dc4062eeb8b0c7208fa
bsamworthdev <bsamworthdev@outlook.com>
2020-05-08 15:03:25 +0000

    Attempt email fix

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Auth\CanResetPassword; 
 
 class User extends Authenticatable
 {
