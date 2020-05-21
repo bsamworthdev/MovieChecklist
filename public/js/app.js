ff532f745f7aa7b190f7a91408be5e46eb330975
Ben Samworth <bsamworthdev@outlook.co.uk>
2020-05-21 22:47:31 +0100

    Force axios to https

--- public/js/app.js
+++ public/js/app.js
@@ -1968,7 +1968,7 @@ __webpack_require__.r(__webpack_exports__);
       //        enable the button
       //    }
 
-      axios.post('/findUserByEmail/', {
+      axios.post('https://moviechecklist.co.uk/findUserByEmail/', {
         email: this.enteredEmail
       }).then(function (response) {
         _this.userExists = response.data;
