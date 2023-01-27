6c2672d60349591a0e4f9b6ddd07ddd0b7199c8a
Ben Samworth <bsamworthdev@outlook.co.uk>
2023-01-27 20:25:37 +0000

    Removed real name field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">
+                            <label for="name" class="col-md-4 col-form-label text-md-right d-none">
                                 {{ __('Real Name') }}&nbsp; &nbsp; 
                             </label>
                             <div class="col-md-6">
