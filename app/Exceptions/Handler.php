84668030c1f40eeb70fde3f62a933e88b7f8add4
Ben Samworth <bsamworthdev@outlook.co.uk>
2023-03-11 20:58:37 +0000

    Redirect to login page if 419 detected

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +37,9 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
+        if ($exception instanceof TokenMismatchException) {
+            return redirect()->route('login');
+        }
         parent::report($exception);
     }
 
